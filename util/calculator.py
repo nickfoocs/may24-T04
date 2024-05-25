5e6e511d7cb00b281f48b2bffd1523b64944b4ad
fcesheng86 <foo.nicholas@gmail.com>
2024-05-25 11:58:33 +0800

    Lesson 3 class practice util

---
+++ util/calculator.py
@@ -0,0 +1,15 @@
+def add(num1, num2):
+    return num1+num2
+
+
+def multiply(num1, num2):
+    return num1*num2
+
+
+def divide(num1, num2):
+    return num1/num2
+
+
+if __name__ == "__main__":
+    result = divide(14, 2)
+    print(result)
